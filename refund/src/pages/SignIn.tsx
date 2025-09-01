8c0ff1bd80f1d0169c55ad3a87cdc6cf703e71cf
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-09-01 15:07:42 -0300

    alterações feitas

--- refund/src/pages/SignIn.tsx
+++ refund/src/pages/SignIn.tsx
@@ -1,7 +1,10 @@
+import { Input } from "../components/Input";
+
 export function SignIn() {
   return (
-    <div>
-      <h1>SignIn</h1>
-    </div>
+    <form className="w-full flex flex-col gap-4">
+      <Input legend="E-mail" type="email" placeholder="Digite seu e-mail" />
+      <Input legend="Senha" type="password" placeholder="Digite sua senha" />
+    </form>
   );
 }
